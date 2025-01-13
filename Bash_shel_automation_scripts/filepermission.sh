3dab5c17f8a4a20933e87f6eaba76f743bea96ec
Suman Kumar S <suman968.sk@gmail.com>
2025-01-14 03:50:49 +0530

    filepermission automation script1

---
+++ Bash_shel_automation_scripts/filepermission.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Enter the Filename:-" File
+echo "The filename is $File"
+search_dir= "/"
+found=$(find $search_dir -type f -name "$File")
+if [ -z "$File" ]
+then
+        echo "The File $File is not found"
+else
+        for File in $found
+        do
+                chmod u+rwx "$File"
+                echo "permission rwx is given for $File"
+        done
+fi
+
